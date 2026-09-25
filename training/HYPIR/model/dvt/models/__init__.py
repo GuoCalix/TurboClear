5b566914971ae8e12b4dc53cb099914227098019
CalixGuo <guocalix@gmail.com>
2026-09-25 11:55:33 +0800

    trim release to ObjectClear inference and training dependencies

--- training/HYPIR/model/dvt/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .neural_feature_field import *
-from .offline_denoiser import *
-from .online_denoiser import *
-from .vit_wrapper import *
--- training/HYPIR/model/dvt/utils/format.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-isort --skip data --skip work_dirs .
-black --exclude '/data/' --exclude '/work_dirs/' . --line-length 100
